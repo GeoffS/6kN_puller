6ba535e69f67f2e0190107e43bbd5933b7900773
Geoff Sobering <gsobering@standardimaging.com>
2025-12-05 14:04:31 -0600

    Made the output a bit more interesting.

--- Acquisition_Application_Arduino/Demo_data_source/Demo_data_source.ino
+++ Acquisition_Application_Arduino/Demo_data_source/Demo_data_source.ino
@@ -5,6 +5,7 @@ void setup()
 
 float x = 0.0;
 float y = 0.0;
+int state = 0;
 
 void loop() 
 {
@@ -12,6 +13,26 @@ void loop()
   Serial.print(",");
   Serial.println(y);
   x += 0.1;
-  y += 0.15;
+  // y += 0.15;
+  switch (state) 
+  {
+    case 0:
+    if(y>20) state=1;
+    y += 0.15;
+    break;
+
+    case 1:
+    if(y<-2) state=2;
+    y -= 0.3;
+    break;
+
+    case 2:
+    if(y>5) state=0;
+    y += 0.3;
+    break;
+
+    default:
+    state = 0;
+  }
   delay(200);
 }
